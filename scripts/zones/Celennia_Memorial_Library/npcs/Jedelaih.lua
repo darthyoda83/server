f78bc4595a7750cf3aa2e06ddfa1d504e0e19d70
Darthyoda83 <40niners83@gmail.com>
2026-02-09 16:42:42 -0500

    af

--- scripts/zones/Celennia_Memorial_Library/npcs/Jedelaih.lua
+++ scripts/zones/Celennia_Memorial_Library/npcs/Jedelaih.lua
@@ -59,8 +59,62 @@ entity.onTrigger = function(player, npc)
 			{11129,325000}, -- Raider's Culottes+2
 			{11149,325000}, -- Raider's Poulaines+2
     }
-
     xi.shop.general(player, stock)
+    end
+    if
+	mJob == xi.job.WAR
+    then
+	local stock =
+    	{
+			-- AF1
+			{11988,35000}, -- Fighter's Torque
+			{16678,35000}, -- Razor Axe
+			{12511,45000}, -- Fighter's Mask
+			{12638,45000}, -- Fighter's Lorica
+			{13961,45000}, -- Fighter's Mufflers
+			{14214,45000}, -- Fighter's Cuisses
+			{14089,45000}, -- Fighter's Calligae
+			{15225,75000}, -- Fighter's Mask+1
+			{14473,75000}, -- Fighter's Lorica+1
+			{14890,75000}, -- Fighter's Mufflers+1
+			{15561,75000}, -- Fighter's Cuisses+1
+			{15352,75000}, -- Fighter's Calligae+1
+			{15871,85000}, -- Warrior's Stone
+			{15072,125000}, -- Warrior's Mask
+			{15087,125000}, -- Warrior's Lorica
+			{15102,125000}, -- Warrior's Mufflers
+			{15117,125000}, -- Warrior's Cuisses
+			{15132,125000}, -- Warrior's Calligae
+			{15245,185000}, -- Warrior's Mask+1
+			{14500,185000}, -- Warrior's Lorica+1
+			{14909,185000}, -- Warrior's Mufflers+1
+			{15580,185000}, -- Warrior's Cuisses+1
+			{15665,185000}, -- Warrior's Calligae+1
+			{10650,235000}, -- Warrior's Mask+2
+			{10670,235000}, -- Warrior's Lorica+2
+			{10690,235000}, -- Warrior's Mufflers+2
+			{10710,235000}, -- Warrior's Cuisses+2
+			{10730,235000}, -- Warrior's Calligae+2
+			{11591,125000}, -- Ravager's Gorget
+			{19253,125000}, -- Ravager's Orb
+			{11703,125000}, -- Ravager's Earring
+			{12008,185000}, -- Ravager's Mask
+			{12028,185000}, -- Ravager's Lorica
+			{12048,185000}, -- Ravager's Mufflers
+			{12068,185000}, -- Ravager's Cuisses
+			{12088,185000}, -- Ravager's Calligae
+			{11164,235000}, -- Ravager's Mask+1
+			{11184,235000}, -- Ravager's Lorica+1
+			{11204,235000}, -- Ravager's Mufflers+1
+			{11224,235000}, -- Ravager's Cuisses+1
+			{11244,235000}, -- Ravager's Calligae+1
+			{11064,325000}, -- Ravager's Mask+2
+			{11084,325000}, -- Ravager's Lorica+2
+			{11104,325000}, -- Ravager's Mufflers+2
+			{11124,325000}, -- Ravager's Cuisses+2
+			{11144,325000}, -- Ravager's Calligae+2
+        }
+	xi.shop.general(player, stock)
     end
 end
 
