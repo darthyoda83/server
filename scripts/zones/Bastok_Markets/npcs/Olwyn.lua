c7a167d084567cc2e6c28ebfbc4507a9eefe09b5
Darthyoda83 <40niners83@gmail.com>
2026-02-09 08:40:38 -0500

    npc

--- scripts/zones/Bastok_Markets/npcs/Olwyn.lua
+++ scripts/zones/Bastok_Markets/npcs/Olwyn.lua
@@ -13,15 +13,15 @@ end
 entity.onTrigger = function(player, npc)
     local stock =
     {
-        { xi.item.FLASK_OF_EYE_DROPS,  2724, },
-        { xi.item.ANTIDOTE,             331, },
-        { xi.item.FLASK_OF_ECHO_DROPS,  840, },
-        { xi.item.POTION,               955, },
-        { xi.item.ETHER,               5025, },
+        { xi.item.FLASK_OF_EYE_DROPS,  2724, 3 },
+        { xi.item.ANTIDOTE,             331, 3 },
+        { xi.item.FLASK_OF_ECHO_DROPS,  840, 2 },
+        { xi.item.POTION,               955, 2 },
+        { xi.item.ETHER,               5025, 1 },
     }
 
     player:showText(npc, zones[xi.zone.BASTOK_MARKETS].text.OLWYN_SHOP_DIALOG)
-    xi.shop.nation(player, stock, nation )
+    xi.shop.nation(player, stock, xi.nation.BASTOK)
 end
 
 return entity
