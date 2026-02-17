ea3da5f9f2da0f464acbf418b372ec138596dc0f
Darthyoda83 <40niners83@gmail.com>
2026-02-17 09:21:37 -0500

    npc

--- scripts/zones/Celennia_Memorial_Library/DefaultActions.lua
+++ scripts/zones/Celennia_Memorial_Library/DefaultActions.lua
@@ -3,7 +3,6 @@
 return {
     ['Andreine']     = { event = 27 },
     ['Hestefa']      = { event = 23 },
-    ['Makel-Pakel']  = { event = 34 },
     ['Trystol']      = { event = 25 },
     ['Vainrachault'] = { event = 24 },
 }
