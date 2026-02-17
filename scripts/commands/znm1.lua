1f306c79f622288c518c6dde92ef77fe03a69946
Darthyoda83 <40niners83@gmail.com>
2026-02-17 00:04:45 -0500

    znm

--- scripts/commands/znm1.lua
+++ scripts/commands/znm1.lua
@@ -14,15 +14,15 @@ commandObj.cmdprops =
 commandObj.onTrigger = function(player)
     local stock =
     {
-        2580,    10000, --   Hellcage Butterfly
-	    2590,    10000, --   Shadeleaf
-	    2600,    10000, --   Golden Teeth
-	    2581,    10000, --   Floral Nectar
-	    2591,    10000, --   Pectin
-	    2601,    10000, --   Greenling
-	    2582,    10000, --   Rodent Cheese
-	    2592,    10000, --   Cog Lubricant
-	    2602,    10000, --   Spoilt Blood
+        {2580,    10000}, --   Hellcage Butterfly
+	    {2590,    10000}, --   Shadeleaf
+	    {2600,    10000}, --   Golden Teeth
+	    {2581,    10000}, --   Floral Nectar
+	    {2591,    10000}, --   Pectin
+	    {2601,    10000}, --   Greenling
+	    {2582,    10000}, --   Rodent Cheese
+	    {2592,    10000}, --   Cog Lubricant
+	    {2602,    10000}, --   Spoilt Blood
     }
 
     xi.shop.general(player, stock)
